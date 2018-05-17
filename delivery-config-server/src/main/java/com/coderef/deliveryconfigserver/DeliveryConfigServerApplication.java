c9949b817f97648ffaac27b06606f3b241299d6a
luizfelipew <luizfelipewendt@gmail.com>
2018-05-17 17:53:48 -0300

    Arrumando configurações no projeto

--- delivery-config-server/src/main/java/com/coderef/deliveryconfigserver/DeliveryConfigServerApplication.java
+++ delivery-config-server/src/main/java/com/coderef/deliveryconfigserver/DeliveryConfigServerApplication.java
@@ -2,8 +2,12 @@ package com.coderef.deliveryconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DeliveryConfigServerApplication {
 
 	public static void main(String[] args) {
