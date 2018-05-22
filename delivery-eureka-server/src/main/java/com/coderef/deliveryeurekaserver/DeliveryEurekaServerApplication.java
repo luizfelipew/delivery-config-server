93881fe8af440542102bcbe5b97047fa74386b33
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-05-22 17:03:50 -0300

    Projeto eureka server

---
+++ delivery-eureka-server/src/main/java/com/coderef/deliveryeurekaserver/DeliveryEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.coderef.deliveryeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryEurekaServerApplication.class, args);
+	}
+}
